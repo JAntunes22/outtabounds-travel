bdc81b87ce2411370aca3e0415a65fa08f7fcea2
JAntunes22 <jpc.antunes22@gmail.com>
2025-05-04 13:50:51 +0200

    Fix multiple UI and routing issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 // src/App.js
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -26,6 +26,5 @@ const App = () => {
     </Router>
   );
 };
-
 
 export default App;
